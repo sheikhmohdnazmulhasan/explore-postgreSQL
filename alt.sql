bcc32636e9ecd6104fbe175f44978f4d0bb0e370
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-17 10:19:33 +0600

    update const in column

--- alt.sql
+++ alt.sql
@@ -8,3 +8,5 @@ ALTER TABLE first_table DROP COLUMN gender
 ALTER Table first_table RENAME age to person_age
 
 ALTER Table first_table alter COLUMN email type VARCHAR(200)
+
+ALTER Table first_table alter COLUMN person_age set Not NULL
