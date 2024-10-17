d9e3809703eb677c0276ba8335c2fff821c1c333
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-17 09:15:31 +0600

    drop column, rename column

--- alt.sql
+++ alt.sql
@@ -2,3 +2,7 @@ SELECT * FROM first_table
 
 ALTER TABLE first_table
 ADD COLUMN gender VARCHAR(255) DEFAULT 'N/A' NOT NULL
+
+ALTER TABLE first_table DROP COLUMN gender
+
+ALTER Table first_table RENAME age to person_age
