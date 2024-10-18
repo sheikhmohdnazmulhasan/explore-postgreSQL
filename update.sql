e97bb044776bc9f0b7fe0ebc6f993262cd695e2b
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-18 21:00:10 +0600

    update multi values in single row

--- update.sql
+++ update.sql
@@ -1,3 +1,8 @@
-UPDATE students SET email = 'xx@zz.com' WHERE student_id = 2
+UPDATE students
+SET
+    email = 'xx@zz.com',
+    first_name = 'name updated'
+WHERE
+    student_id = 2
 
 SELECT * FROM students
