f7f99244bf3e9f28e3dd9f2524549a62a6ea1fae
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-19 21:56:42 +0600

    having and groupby

--- groupBy.sql
+++ groupBy.sql
@@ -1,3 +1,24 @@
 SELECT * FROM students
 
-SELECT age, avg(age) FROM students GROUP BY age
+SELECT age, avg(age)
+FROM students
+GROUP BY
+    age
+HAVING
+    avg(age) < 20
+
+SELECT extract(
+        year
+        from dob::date
+    ) as birth_year, count(*)
+FROM students
+GROUP BY
+    birth_year
+
+SELECT extract(
+        year
+        FROM dob::date
+    ) as birth_year, count(*)
+FROM students
+GROUP BY
+    birth_year
