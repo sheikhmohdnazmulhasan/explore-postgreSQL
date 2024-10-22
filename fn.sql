f5e5aa6d9e0f737346f87f82cb405d21651fc4b2
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-22 18:58:31 +0600

    function with parameater

--- fn.sql
+++ fn.sql
@@ -6,3 +6,13 @@ CREATE Function student_count ()
  $$
 
 SELECT student_count ()
+
+CREATE Function delete_student_by_id (p_student_id int) 
+RETURNS void
+LANGUAGE SQL as
+$$
+DELETE COLUMN FROM students
+WHERE student_id = p_student_id
+$$
+
+SELECT * from students
