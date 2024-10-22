8ac45d969934a614f0fcaaa76f64a887c169dcc4
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-22 23:55:06 +0600

    created procedure with perameter

--- psdre.sql
+++ psdre.sql
@@ -2,8 +2,20 @@ CREATE Procedure remove_student()
 LANGUAGE plpgsql AS
 $$
 BEGIN
-DELETE FROM students WHERE student_id = 3
+DELETE FROM students WHERE student_id = 3;
 END
 $$
 
 SELECT * FROM students
+
+CALL remove_student ()
+
+CREATE Procedure remove_student_by_Id(p_student_id INT)
+LANGUAGE plpgsql AS
+$$
+BEGIN
+DELETE FROM students WHERE student_id = p_student_id;
+END
+$$
+
+CALL remove_student_by_id (10)
