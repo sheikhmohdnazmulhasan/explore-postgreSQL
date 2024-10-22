66a88724c0e7d8b9eac884b64c6acc4347f1d944
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-22 12:45:19 +0600

    created view

--- view.sql
+++ view.sql
@@ -1,3 +1,12 @@
 SELECT * FROM students
 
-SELECT blood_group, FROM students GROUP BY on blood_group
+SELECT blood_group, round(avg(age))
+FROM students
+GROUP BY
+    blood_group;
+
+CREATE View stdnts_avg_age as
+SELECT blood_group, round(avg(age))
+FROM students
+GROUP BY
+    blood_group
