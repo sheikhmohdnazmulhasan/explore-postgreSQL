821ab295e9a100cc75a10aa00cd9b12a5473f202
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-10-23 00:11:30 +0600

    created procedure with variable

--- psdre.sql
+++ psdre.sql
@@ -19,3 +19,14 @@ END
 $$
 
 CALL remove_student_by_id (10)
+
+CREATE Procedure remove_student_by_id_var (p_student_id) 
+LANGUAGE plpgsql AS
+$$
+DECLARE targeted_student_var INT;
+
+BEGIN
+SELECT student_id  FROM students WHERE student_id = p_student_id;
+
+END
+$$
